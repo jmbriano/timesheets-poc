ce0943bdbe59ba0ce70bafaf5e1e78b11db4872c
gebat <germanbatiston@gmail.com>
2017-04-14 13:26:35 +0200

    GB | FreshbookReportReader

---
+++ src/main/java/be/lemonade/timesheet/FreshbookReportReader.java
@@ -0,0 +1,29 @@
+package be.lemonade.timesheet;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FreshbookReportReader {
+
+    public List<FreshbookTimeEntry> parseRecords(String filename) throws IOException {
+
+        List<FreshbookTimeEntry> records = new ArrayList<FreshbookTimeEntry>();
+        Reader fr = new FileReader(filename);
+
+        for (CSVRecord currentEntry : CSVFormat.EXCEL.withHeader().parse(fr)) {
+            FreshbookTimeEntry newEntry = new FreshbookTimeEntry(currentEntry.get("Team"), currentEntry.get("Client"),
+                    currentEntry.get("Project"), currentEntry.get("Task"), currentEntry.get("Date"), currentEntry.get("Hours"));
+            records.add(newEntry);
+        }
+        return records;
+    }
+
+}
